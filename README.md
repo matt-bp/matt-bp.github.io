27aceb442ecd7678e08268fb3da6702329bf5660
matt-bp <matt.bishop@readinghorizons.com>
2023-05-31 16:47:52 -0600

    Added in dark/light mode switcher, and adding dev tool integration.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Matt's Blog
 
-This is the source code for my blog. 
+This is the source code for my blog.
 
 Written and maintained for me.
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
 export async function get() {
   return rss({
     title: "Matt Bishop's | Blog",
-    description: "My journey learning Astro",
-    site: "https://matt-bp.github.io",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-    customData: `<language>en-us</language>`,
-  });
+    description: 'My journey learning Astro',
+    site: 'https://matt-bp.github.io',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`
+  })
 }
