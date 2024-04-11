66499af87693686c25a28c3f6589fc27f63f7d11
Matt Bishop <mattsbishop1@gmail.com>
2024-04-11 10:27:01 -0600

    Updating code styling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://matt-bp.github.io",
+  markdown: {
+    shikiConfig: {
+      theme: "monokai",
+    }
+  }
 });
